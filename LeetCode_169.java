1ee9fca7c8c7afe6b6a7236530999bfdbebd8858
deepchandra633 <kushwahdeepchandra640@gmail.com>
2026-03-22 22:08:58 +0530

    Mazority Element

---
+++ LeetCode_169.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       
+        return ME(nums);
+    }
+     public static int ME(int[] nums) {
+        int n = nums.length;
+        int count=0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        for (int key : map.keySet()) {
+            if(map.get(key)>n/2){
+                return key;
+            }
+        }
+
+        return -1;
+    }
+}
