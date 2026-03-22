ee90058f3d482e9e55a7fbee66098741142c74e6
deepchandra633 <kushwahdeepchandra640@gmail.com>
2026-03-22 22:05:55 +0530

    Maximum Product SubArray

---
+++ LeetCode_152.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int maxpro=Integer.MIN_VALUE;
+        int pro=1;
+        for(int i=0;i<n;i++){
+            pro=pro*nums[i];
+            
+            if(pro>maxpro){
+                maxpro=pro;
+            }
+            if( pro==0){
+                pro=1;
+            }
+            
+        }
+        pro=1;
+        for(int i=n-1;i>0;i--){
+            pro=pro*nums[i];
+            
+            if(pro>maxpro){
+                maxpro=pro;
+            }
+            if( pro==0){
+                pro=1;
+            }
+            
+        }
+        return maxpro;
+    }
+}
