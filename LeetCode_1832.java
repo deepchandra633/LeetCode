8616e233f5e23472eaf0c73597b22bc37ad6bd50
deepchandra633 <kushwahdeepchandra640@gmail.com>
2026-03-23 23:18:22 +0530

    Check If The Sentence Is pangram

---
+++ LeetCode_1832.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        Set<Character> set =new HashSet<>();
+        for(int i=0;i<sentence.length();i++){
+            set.add(sentence.charAt(i));
+        }
+        if(set.size()==26){
+            return true;
+        }
+            return false;
+
+    }
+}
